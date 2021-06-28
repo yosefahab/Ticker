cb6ec398a3b3d14d40948890d65cbf9fd95a873c
Youssef <YoussefAhab@outlook.com>
2021-06-28 15:57:27 +0200

    added functionality to progress bar

--- Ticker.py
+++ Ticker.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 
 
 class counter:
@@ -24,25 +25,18 @@ class counter:
             self.window, background='#2f3745', height=300, width=110)
         self.mainframe.grid(row=0, column=0)
 
-        self.start = tkinter.Button(self.mainframe,
-                                    text='Start',
-                                    width=8,
-                                    bg="#2f3745",
-                                    fg='white',
-                                    activebackground='#313b47',
-                                    activeforeground='white',
+        self.start = tkinter.Button(self.mainframe, text='Start',
+                                    width=8, borderwidth=0.01,
                                     font=18,
-                                    borderwidth=0.01,
+                                    bg="#2f3745", fg='white',
+                                    activebackground='#313b47', activeforeground='white',
                                     command=self.Start)
         self.reset = tkinter.Button(self.mainframe,
                                     text='Reset',
-                                    width=8,
-                                    bg="#2f3745",
-                                    fg='white',
-                                    activebackground='#313b47',
-                                    activeforeground='white',
+                                    width=8, borderwidth=0.01,
                                     font=18,
-                                    borderwidth=0.01,
+                                    bg="#2f3745", fg='white',
+                                    activebackground='#313b47', activeforeground='white',
                                     state='disabled',
                                     command=self.Reset)
         self.start.grid(row=3, column=1)
@@ -51,10 +45,8 @@ class counter:
         self.num = tkinter.Label(self.mainframe,
                                  textvariable=self.counter,
                                  font=18,
-                                 bd=1,
-                                 width=10,
-                                 bg="#2f3745",
-                                 fg='white',
+                                 bd=1, width=10,
+                                 bg="#2f3745", fg='white',
                                  pady=6)
         self.num.grid(row=0, column=2)
 
@@ -83,28 +75,54 @@ class counter:
         self.start['state'] = 'normal'
         self.reset['state'] = 'disabled'
 
+    def updateProg(self):
+        self.pBar['value'] += 1
+        if self.pBar['value'] < self.pBar['maximum']:
+            self.breakWin.after(1000, self.updateProg)
+        else:
+            self.breakWin.destroy()
+
     def Break(self):
         self.window.withdraw()
-        breakWin = tkinter.Toplevel(self.window)
-        breakWin.iconbitmap("clock.ico")
-        breakWin.title("Break Time")
-        breakWin.geometry("600x220")
-        breakWin.attributes('-alpha', 0.95)
-        breakWin.resizable(False, False)
-        breakWin.attributes('-fullscreen', 1)
-
-        fm = tkinter.Frame(breakWin, bg="gray13")
-        fm.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+        self.breakWin = tkinter.Toplevel(self.window)
+        self.breakWin.iconbitmap("clock.ico")
+        self.breakWin.title("Break Time")
+        self.breakWin.attributes('-alpha', 0.95)
+        self.breakWin.resizable(False, False)
+        self.breakWin.attributes('-fullscreen', 1)
+        self.breakWin.grab_set()
+        fm = tkinter.Frame(self.breakWin, bg="gray13")
+        fm.pack(fill=tkinter.BOTH, expand=1)
 
         brTxt = tkinter.Label(fm, text="Time for a break", font=(
             "Arial", 25), fg="white", bg="gray13")
-        brTxt.pack(side=tkinter.TOP, expand=tkinter.YES)
+        brTxt.pack(side=tkinter.TOP, expand=1)
+
+        txt = tkinter.Label(fm, text="for 20 seconds, look at something 20 feet away", font=(
+            "Arial", 20), fg="#706F6C", bg="gray13")
+        txt.pack(side=tkinter.TOP, expand=1)
+
+        s = ttk.Style()
+        s.theme_use("clam")
+        s.configure("Horizontal.TProgressbar",
+                    thickness=5,
+                    troughcolor='gray16',
+                    bordercolor='gray16',
+                    lightcolor='#526882',
+                    darkcolor='#526882',
+                    foreground='#526882',
+                    background='#526882')
+        self.pBar = ttk.Progressbar(fm, orient=tkinter.HORIZONTAL, style="Horizontal.TProgressbar",
+                                    mode='determinate', maximum=30,
+                                    length=300, value=-1)
+        self.pBar.pack(side=tkinter.TOP, expand=1)
 
         exitBtn = tkinter.Button(fm, width=10, text='Exit now', borderwidth=0.01, font=('Arial 12 underline'),
-                                 command=breakWin.destroy, activebackground='gray13',
+                                 command=self.breakWin.destroy, activebackground='gray13',
                                  activeforeground='white', bg="gray13", fg='white', relief=tkinter.FLAT)
-        exitBtn.pack(side=tkinter.TOP, expand=tkinter.YES)
+        exitBtn.pack(side=tkinter.TOP, expand=1)
         exitBtn.bind("<Destroy>", lambda x: self.window.deiconify())
+        self.updateProg()
 
 
 def main():
